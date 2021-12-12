eafc92fcb55816905fc15bceef5600967450f4e4
EXP <289065406@qq.com>
2021-12-13 00:23:10 +0800

    1.0.4 降低依赖版本

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.3',  # Required. eg. 1.2.3
+    version='1.0.4',  # Required. eg. 1.2.3
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -143,7 +143,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pyyaml==6.0'],  # Optional
+    install_requires=['pyyaml>=5.1'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
